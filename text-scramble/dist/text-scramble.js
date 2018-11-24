c4f61e1264d381ff995159866ead0e175a941db4
bahrus <anderson.bruce.b@gmail.com>
2018-11-24 17:56:44 -0500

    ...

---
+++ text-scramble/dist/text-scramble.js
@@ -0,0 +1,190 @@
+export function define(custEl) {
+    let tagName = custEl.is;
+    if (customElements.get(tagName)) {
+        console.warn('Already registered ' + tagName);
+        return;
+    }
+    customElements.define(tagName, custEl);
+}
+export const disabled = 'disabled';
+/**
+ * Base class for many xtal- components
+ * @param superClass
+ */
+export function XtallatX(superClass) {
+    return class extends superClass {
+        constructor() {
+            super(...arguments);
+            this._evCount = {};
+        }
+        static get observedAttributes() {
+            return [disabled];
+        }
+        /**
+         * Any component that emits events should not do so if it is disabled.
+         * Note that this is not enforced, but the disabled property is made available.
+         * Users of this mix-in should ensure not to call "de" if this property is set to true.
+         */
+        get disabled() {
+            return this._disabled;
+        }
+        set disabled(val) {
+            this.attr(disabled, val, '');
+        }
+        /**
+         * Set attribute value.
+         * @param name
+         * @param val
+         * @param trueVal String to set attribute if true.
+         */
+        attr(name, val, trueVal) {
+            const v = val ? 'set' : 'remove'; //verb
+            this[v + 'Attribute'](name, trueVal || val);
+        }
+        /**
+         * Turn number into string with even and odd values easy to query via css.
+         * @param n
+         */
+        to$(n) {
+            const mod = n % 2;
+            return (n - mod) / 2 + '-' + mod;
+        }
+        /**
+         * Increment event count
+         * @param name
+         */
+        incAttr(name) {
+            const ec = this._evCount;
+            if (name in ec) {
+                ec[name]++;
+            }
+            else {
+                ec[name] = 0;
+            }
+            this.attr('data-' + name, this.to$(ec[name]));
+        }
+        attributeChangedCallback(name, oldVal, newVal) {
+            switch (name) {
+                case disabled:
+                    this._disabled = newVal !== null;
+                    break;
+            }
+        }
+        /**
+         * Dispatch Custom Event
+         * @param name Name of event to dispatch ("-changed" will be appended if asIs is false)
+         * @param detail Information to be passed with the event
+         * @param asIs If true, don't append event name with '-changed'
+         */
+        de(name, detail, asIs) {
+            const eventName = name + (asIs ? '' : '-changed');
+            const newEvent = new CustomEvent(eventName, {
+                detail: detail,
+                bubbles: true,
+                composed: false,
+            });
+            this.dispatchEvent(newEvent);
+            this.incAttr(eventName);
+            return newEvent;
+        }
+        /**
+         * Needed for asynchronous loading
+         * @param props Array of property names to "upgrade", without losing value set while element was Unknown
+         */
+        _upgradeProperties(props) {
+            props.forEach(prop => {
+                if (this.hasOwnProperty(prop)) {
+                    let value = this[prop];
+                    delete this[prop];
+                    this[prop] = value;
+                }
+            });
+        }
+    };
+}
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
+import { define } from 'xtal-latx/define.js';
+// Based on https://codepen.io/soulwire/pen/mErPAK
+export class TextScramble extends XtallatX(HTMLElement) {
+    //resolve: any;
+    constructor() {
+        super();
+        this.chars = '!<>-_\\/[]{}—=+*^?#________';
+        this.update = this.update.bind(this);
+    }
+    static get is() {
+        return 'co-depends-text-scramble';
+    }
+    static get observedAttributes() {
+        return super.observedAttributes.concat(['text']);
+    }
+    connectedCallback() {
+        this._upgradeProperties(['text']);
+    }
+    attributeChangedCallback(n, ov, nv) {
+        super.attributeChangedCallback(n, ov, nv);
+        switch (n) {
+            case 'text':
+                this._text = nv;
+                this.setText(ov, nv);
+                break;
+        }
+    }
+    get text() {
+        return this._text;
+    }
+    set text(nv) {
+        this.attr('text', nv);
+    }
+    setText(oldText, newText) {
+        if (oldText === null)
+            oldText = '';
+        const length = Math.max(oldText.length, newText.length);
+        //const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || '';
+            const to = newText[i] || '';
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
+            this.queue.push({ from, to, start, end });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        //return promise;
+    }
+    update() {
+        let output = '';
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            }
+            else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dud">${char}</span>`;
+            }
+            else {
+                output += from;
+            }
+        }
+        this.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.de('text-setting-complete', {}, true);
+        }
+        else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
+define(TextScramble);
