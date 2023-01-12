46a0b4d9665b037b06126e26e88764bc61680023
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-11 21:24:55 -0500

    ...

---
+++ responsive-honeycomb/make.ts
@@ -0,0 +1,17 @@
+import {EndUserProps as BeDefEUP} from 'be-definitive/types';
+import {EndUserProps as BeFdEUP} from 'be-ferried/types';
+
+export const make = {
+    ":host": {
+        be: 'definitive',
+        having: {
+
+        } as BeDefEUP,
+    },
+    slot: {
+        be: 'ferried',
+        having: {
+            xsltHref: 'co-depends/responsive-honeycomb/ferry.xslt'
+        } as BeFdEUP
+    } 
+}
